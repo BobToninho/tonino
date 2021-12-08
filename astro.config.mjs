cd2fe0b58b7be24bf008beb64dedff0fdc3d105a
Roberto Tonino <roberto.tonino5@gmail.com>
2021-12-08 13:13:11 +0100

    feat!: changed site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
 	buildOptions: {
-		site: 'http://alternative.tonino.xyz', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+		site: 'https://tonino.xyz', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
 		sitemap: true // Generate sitemap (set to "false" to disable)
 	},
 	devOptions: {},
