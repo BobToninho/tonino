eb02ce7212f224b33dd00489959b03a72b7f4549
Roberto Tonino <roberto.tonino5@gmail.com>
2021-12-10 19:16:18 +0100

    feat: BaseDate client-side component

---
+++ src/components/BaseDate.vue
@@ -0,0 +1,25 @@
+<template>
+	<time :datetime="props.date">{{ dateFormatted }}</time>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+	// Date in ISO format
+	date: {
+		type: String,
+		required: true,
+	},
+})
+
+const options: Intl.DateTimeFormatOptions = {
+	weekday: 'long',
+	year: 'numeric',
+	month: 'long',
+	day: 'numeric',
+}
+const date = new Date(props.date)
+const dateFormatted = date.toLocaleString(window.navigator.language, options)
+</script>
+
+<style scoped>
+</style>
