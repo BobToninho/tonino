f465b3d399207cf0897d50c7d13b23eff86626ef
Roberto Tonino <roberto.tonino5@gmail.com>
2022-04-10 13:27:49 +0200

    feat: migrating to astro 1.0.0 beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,10 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-	buildOptions: {
-		site: 'https://tonino.xyz', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-		sitemap: true, // Generate sitemap (set to "false" to disable)
-	},
-	devOptions: {},
-	renderers: ['@astrojs/renderer-vue'],
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://tonino.xyz',
+	integrations: [sitemap()],
 	vite: {
 		define: {
 			__VERSION__: JSON.stringify(process.env.npm_package_version),
