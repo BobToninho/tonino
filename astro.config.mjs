eda3d20b377e765b24c3d2f7b379c3e33d543b16
Roberto Tonino <roberto.tonino5@gmail.com>
2022-04-10 16:36:59 +0200

    feat: added tailwind integration and restored old syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tonino.xyz',
-	integrations: [sitemap()],
+	integrations: [
+		sitemap(),
+		tailwind({
+			config: {
+				applyAstroPreset: false,
+			},
+		}),
+	],
+	markdown: {
+		syntaxHighlight: 'prism',
+	},
 	vite: {
 		define: {
 			__VERSION__: JSON.stringify(process.env.npm_package_version),
