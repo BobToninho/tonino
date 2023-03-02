a7dea65a0045ce7210f7d4a65d53e19bbca34553
Roberto Tonino <roberto.tonino5@gmail.com>
2023-03-02 17:07:40 +0100

    feat: render all content in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,19 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import sanitizeHtml from 'sanitize-html'
 
 export async function get(context) {
+	const blogImportResult = await import.meta.glob('./blog/*.{md, mdx}', { eager: true })
+	const blogPosts = Object.values(blogImportResult)
+
 	return rss({
 		title: "Roberto Tonino's web corner",
 		description: "Roberto Tonino's web corner",
 		site: context.site,
-		items: await pagesGlobToRssItems(import.meta.glob('./blog/*.{md, mdx}')),
+		items: blogPosts.map(post => ({
+			link: post.url,
+			content: sanitizeHtml(post.compiledContent()),
+			...post.frontmatter,
+		})),
 		customData: '<language>en-us</language>',
 	})
 }
