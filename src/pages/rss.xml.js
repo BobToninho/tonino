4ae0b96129024b58dcbc59a18ff3506ba1a9f0fe
Roberto Tonino <roberto.tonino5@gmail.com>
2023-03-24 17:26:41 +0100

    style: add rss stylesheet

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -14,6 +14,7 @@ export async function get(context) {
 			content: sanitizeHtml(post.compiledContent()),
 			...post.frontmatter,
 		})),
+		stylesheet: '/pretty-feed-v3.xsl',
 		customData: '<language>en-us</language>',
 	})
 }
