c28bbb4dbcbbbd8d61d05b063b314157cc50651f
Roberto Tonino <58312518+BobToninho@users.noreply.github.com>
2024-01-28 23:23:30 +0100

    fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,12 @@
-import rss from '@astrojs/rss'
-import sanitizeHtml from 'sanitize-html'
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
 
 export async function GET(context) {
-	const blogImportResult = await import.meta.glob('./blog/*.{md, mdx}', { eager: true })
-	const blogPosts = Object.values(blogImportResult)
-
 	return rss({
 		title: "Roberto Tonino's web corner",
 		description: "Roberto Tonino's web corner",
 		site: context.site,
-		items: blogPosts.map(post => ({
-			link: post.url,
-			content: sanitizeHtml(post.compiledContent()),
-			...post.frontmatter,
-		})),
+		items: await pagesGlobToRssItems(import.meta.glob(['./blog/*.md', './blog/*.mdx'])),
 		stylesheet: '/pretty-feed-v3.xsl',
-		customData: '<language>en-us</language>',
+		customData: `<language>en-us</language>`,
 	})
 }
